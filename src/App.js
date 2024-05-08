a5e1792af45474edc246e6347b8d12b2da420aa3
Jean Carlos Moissa <jeanmoissa@gmail.com>
2024-05-08 18:58:11 -0300

    main
    Envio inicial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import icon from './imagens/computer.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>
+        <img  src={icon}></img>
+        <p><strong>Empresa</strong>Teste</p>
+      </header>
+
+      <body className='body'>
+        
+      </body>
+    </div>
+  );
+}
+
+export default App;
